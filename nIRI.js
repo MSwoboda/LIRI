8163335732c3117e412631991edc54739a86afde
swoboda <michal.swoboda@outlook.com>
2019-10-15 18:29:54 -0400

    Update nIRI.js

--- nIRI.js
+++ nIRI.js
@@ -3,8 +3,8 @@ let keys = require("./keys.js");
 
 // Load the inquirer package
 const axios = require("axios");
-//var Spotify = require('node-spotify-api');
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 let moment = require("moment");
 
@@ -18,35 +18,35 @@ let IN_ARG = process.argv[3];
 
 switch (IN_COM) {
     case `concert-this`:
-            console.log(`concert-this`);
+        console.log(`concert-this`);
 
         break;
     case `spotify-this-song`:
-            console.log(`spotify-this-song`);
-            // spotify.search({ type: 'track', query: IN_ARG }, function(err, data) {
-            //     if (err) {
-            //       return //console.log('Error occurred: ' + err);
-            //     }
-               
-            //  // console.log(JSON.stringify(data.tracks.items[0].artists[0].name,null, 2));
-            // //   console.log("Song Name: "+JSON.stringify(data.tracks.items[0].artists[0].name,null, 2));
-            // //   console.log("Author: "+JSON.stringify(data.tracks.items[0].artists[0].name,null, 2));
+        console.log(`spotify-this-song`);
+        spotify.search({ type: 'track', query: IN_ARG }, function(err, data) {
+            if (err) {
+                return //console.log('Error occurred: ' + err);
+            }
 
-              
-            //   });
+            console.log("Author: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+            console.log("Song Name: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+            // console.log("Author: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+
+
+        });
 
         break;
     case `movie-this`:
-            console.log(`movie-this`);
+        console.log(`movie-this`);
 
         break;
     case `do-what-it-says`:
-            console.log(`do-what-it-says`);
+        console.log(`do-what-it-says`);
 
         break;
 
     default:
         console.log('Command not recognized');
-        
+
         break;
 }
